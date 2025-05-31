64347564747afcb50d8fe26d8bce29681c0cf37a
MarcelSunyer <45765502+MarcelSunyer@users.noreply.github.com>
2025-05-31 16:20:10 +0200

    Arrow Look At

---
+++ Tale of the Ripper treasure/Assets/Scripts/ArrowLookAt.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowLookAt : MonoBehaviour
+{
+    public GameObject targetPoint;
+
+    void Update()
+    {
+        // Calcula la dirección desde la flecha hacia el punto objetivo
+        Vector3 direction = targetPoint.transform.position - transform.position;
+
+        // Si la dirección no es cero, rota la flecha hacia ese punto
+        if (-direction != Vector3.zero)
+        {
+            Quaternion lookRotation = Quaternion.LookRotation( new Vector3(-direction.x, direction.y, direction.z));
+            transform.rotation = lookRotation;
+        }
+    }
+}
