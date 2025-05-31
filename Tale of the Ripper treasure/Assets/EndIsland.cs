127d9841eee29f77d4b73c03c77a842c45844dce
MarcelSunyer <45765502+MarcelSunyer@users.noreply.github.com>
2025-05-31 17:43:04 +0200

    Fix de isals y ocsas

---
+++ Tale of the Ripper treasure/Assets/EndIsland.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndIsland : MonoBehaviour
+{
+    public bool mission_completed;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player" && mission_completed)
+        {
+            SceneManager.LoadScene("Isle1_Done");
+        }
+        if(other.gameObject.tag == "Player" && !mission_completed)
+        {
+            SceneManager.LoadScene("Waves");
+        }
+    }
+}
