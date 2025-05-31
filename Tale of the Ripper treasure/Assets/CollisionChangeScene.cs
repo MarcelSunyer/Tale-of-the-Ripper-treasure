6af57630a4ad9af481892bb3db13ce8afb652d8e
MarcelSunyer <45765502+MarcelSunyer@users.noreply.github.com>
2025-05-31 13:32:12 +0200

    Updarte Main Map

---
+++ Tale of the Ripper treasure/Assets/CollisionChangeScene.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CollisionChangeScene : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Floor Colliders") 
+        {
+            Debug.Log("Josemaria");
+            SceneManager.LoadScene("OnTheIsland");
+        }
+    }
+   
+}
